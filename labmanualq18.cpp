0dd8ed020daa317c0fb9f479c36441f0a1653b0a
khatanagaurav <khatanagaurav88@gmail.com>
2026-05-08 22:11:51 +0530

    commit

---
+++ labmanualq18.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    string rev = "";
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+
+    if (str == rev) {
+        cout << "Palindrome";
+    }
+    else {
+        cout << "Not a Palindrome";
+    }
+
+    return 0;
+}
