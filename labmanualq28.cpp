5f4e169391f30536afcaecc43d1b8ee767b489c6
khatanagaurav <khatanagaurav88@gmail.com>
2026-05-08 22:19:16 +0530

    commit

---
+++ labmanualq28.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    int evenSum = 0, oddSum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] % 2 == 0) {
+            evenSum += arr[i];
+        }
+        else {
+            oddSum += arr[i];
+        }
+    }
+
+    cout << "Sum of Even Numbers = " << evenSum << endl;
+    cout << "Sum of Odd Numbers = " << oddSum;
+
+    return 0;
+}
