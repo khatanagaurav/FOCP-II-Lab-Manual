2062ddf34fccaef05d80db6d0003b418af76d006
khatanagaurav <khatanagaurav88@gmail.com>
2026-05-08 22:07:00 +0530

    commit

--- labmanualq12.cpp
+++ labmanualq12.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+        cout << "Invalid Triangle";
+    }
+    else if (a == b && b == c) {
+        cout << "Equilateral Triangle";
+    }
+    else if (a == b || b == c || a == c) {
+        cout << "Isosceles Triangle";
+    }
+    else {
+        cout << "Scalene Triangle";
+    }
+
+    return 0;
+}
