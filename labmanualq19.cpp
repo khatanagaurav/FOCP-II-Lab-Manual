294402eaf174f482095dec900d8774b074873206
khatanagaurav <khatanagaurav88@gmail.com>
2026-05-08 22:12:34 +0530

    commit

---
+++ labmanualq19.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start, end;
+
+    cin >> start >> end;
+
+    for (int i = start; i <= end; i++) {
+        if (i <= 1) {
+            continue;
+        }
+
+        bool prime = true;
+
+        for (int j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
